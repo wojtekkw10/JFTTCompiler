9a2c24f2a4a3fcfc9aaf44337b1657deb7b32dc4
Wojciech Kwaśniewicz <wojtek.kwasniewicz@gmail.com>
2020-04-13 13:25:07 +0200

    Recompiled the compiler [FIX] Recompiled the compiler

--- vm/parser.cc
+++ vm/parser.cc
@@ -1,4 +1,4 @@
-/* A Bison parser, made by GNU Bison 3.4.  */
+/* A Bison parser, made by GNU Bison 3.4.1.  */
 
 /* Bison implementation for Yacc-like parsers in C
 
@@ -48,7 +48,7 @@
 #define YYBISON 1
 
 /* Bison version.  */
-#define YYBISON_VERSION "3.4"
+#define YYBISON_VERSION "3.4.1"
 
 /* Skeleton name.  */
 #define YYSKELETON_NAME "yacc.c"
